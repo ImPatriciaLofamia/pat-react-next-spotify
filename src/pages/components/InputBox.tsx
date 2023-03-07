0f5eb6c96bb9556ef692937fe0c2c1bcac38162a
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-03-07 13:52:50 +0800

    STYLE: Add style in login page

--- src/pages/components/InputBox.tsx
+++ src/pages/components/InputBox.tsx
@@ -1,10 +1,14 @@
 interface InputBoxProps {
-    text: string
+  text: string;
 }
-const InputBox = (props:InputBoxProps) => {
-    return(
-        <input className="bg-white border pb-3 pt-3 border-gray-400 text-center text-base text-black font-medium rounded-lg" placeholder={props.text} type="text"/>
-    )
-}
+const InputBox = (props: InputBoxProps) => {
+  return (
+    <input
+      className="bg-white border pb-3 pt-3 border-gray-400 text-center text-base text-black font-medium rounded-lg"
+      placeholder={props.text}
+      type="text"
+    />
+  );
+};
 
 export default InputBox;
