6995b8a5600eee39af1a84cb87aa107bce8031d4
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-03-08 15:20:10 +0800

    TEST: Test side bar navi with bg color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Inter } from "next/font/google";
+import SideBarNav from "./components/SideBarNav";
 import { Login } from "./Login";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <Login/>
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="">
+        <SideBarNav/>
+        {/* This is center */}
+      </main>
+      <div>
+        {/* This is the player */}
+      </div>
+    </div>
+    
   );
 }
