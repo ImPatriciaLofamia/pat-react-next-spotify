94ccb022b5ba6dad95d9c92675353c9c9b67d582
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-03-08 08:45:28 +0800

    FEAT: Add Sidebar Navigation

--- src/pages/components/SideBarNav.tsx
+++ src/pages/components/SideBarNav.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import { 
-  HomeIcon, 
-  BuildingLibraryIcon, 
-  MagnifyingGlassIcon, 
-  PlusCircleIcon,
-  HeartIcon,
-  RssIcon
-} from "@heroicons/react/24/outline";
+import { RssIcon ,HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon } from "@heroicons/react/outline";
 
 function SideBarNav() {
   return (
@@ -17,29 +10,31 @@ function SideBarNav() {
           <p>Home</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <MagnifyingGlassIcon className="w-5 h-5 " />
+          <SearchIcon className="w-5 h-5 " />
           <p>Search</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <BuildingLibraryIcon className="w-5 h-5 " />
+          <LibraryIcon className="w-5 h-5 " />
           <p>My Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900"></hr>
+
         <button className="flex items-center space-x-2 hover:text-white">
           <PlusCircleIcon className="w-5 h-5 " />
           <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HeartIcon className="w-5 h-5 " />
-          <p>Liked Songs</p>
+          <p>Liked Song</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <RssIcon className="w-5 h-5 " />
-          <p>Episodes</p>
+          <p>Episode</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900"></hr>
 
         {/* This is where playlist would be rendered */}
+        <p className=" cursor-pointer hover:text-white">Playlist name...</p>
       </div>
     </div>
   );
