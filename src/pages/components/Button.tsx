3b6f0cfb919e84e8d34f8f868de977f9ddd05db1
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-03-08 13:36:12 +0800

    REFACTOR: custom component for button and input

--- src/pages/components/Button.tsx
+++ src/pages/components/Button.tsx
@@ -1,20 +1,20 @@
-import React from "react"
+import React from "react";
 
 interface ButtonProps {
-    buttonName: string,
-    icon: any,
-    className: string
+  buttonName: string;
+  icon: any;
+  className: string;
+  onClick?: () => void
 }
 
-const Button = ({className,buttonName, icon}:ButtonProps) => {
-    
-    return(
-        <button
-        className={`${className} flex justify-center text-1xl font-bold border w-4/12 border-gray-400  py-1 px-3 rounded-full pb-3 pt-3`}>
-            <span className="pr-1">{icon}</span>
-            <span>{buttonName}</span>
-        </button>
-
-    )
-}
+const Button = ({ className, buttonName, icon, onClick }: ButtonProps) => {
+  return (
+    <button
+      className={`${className} flex justify-center text-1xl font-bold border w-4/12 border-gray-400  py-1 px-3 rounded-full pb-3 pt-3`}
+    >
+      <span className="pr-1">{icon}</span>
+      <span>{buttonName}</span>
+    </button>
+  );
+};
 export default Button;
--- src/pages/components/InputBox.tsx
+++ src/pages/components/InputBox.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 
 interface InputBoxProps {
-  text: string;
-  className: string;
+  placeholder?: string;
+  className?: string;
+  type?: string;
+  value?: string;
+  onChange?: (text: string) => void;
+  label?: string;
 }
-const InputBox = ({ className, props }: InputBoxProps) => {
+const InputBox = ({
+  type,
+  label,
+  className,
+  placeholder,
+  value,
+  onChange = () => null,
+}: InputBoxProps) => {
   return (
-    <input
-      className={`${className} bg-white border pb-3 pt-3 mb-1 w-full border-gray-400 text-center text-base text-black font-medium rounded-lg`}
-      placeholder={props}
-      type="text"
-    />
+    <>
+      {label && <p>{label}</p>}
+      <input
+        className={`${className} bg-white border pb-3 pt-3 mb-1 w-full border-gray-400 text-base text-black font-medium rounded-lg`}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </>
   );
 };
 
