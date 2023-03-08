8a32a1fc6d8c6b18cf2922cf2f6420b5f8a3d661
Patricia Lofamia <101615967+ImPatriciaLofamia@users.noreply.github.com>
2023-03-08 15:07:20 +0800

    REFACTOR: components for button and login page

--- src/pages/components/Button.tsx
+++ src/pages/components/Button.tsx
@@ -2,18 +2,29 @@ import React from "react";
 
 interface ButtonProps {
   buttonName: string;
-  icon: any;
-  className: string;
-  onClick?: () => void
+  icon?: any;
+  className?: string;
+  onClick?: () => void;
+  success?: boolean;
+  primary?: boolean;
 }
-
-const Button = ({ className, buttonName, icon, onClick }: ButtonProps) => {
+const Button = ({
+  className,
+  buttonName,
+  icon,
+  onClick,
+  success,
+  primary,
+}: ButtonProps) => {
   return (
     <button
-      className={`${className} flex justify-center text-1xl font-bold border w-4/12 border-gray-400  py-1 px-3 rounded-full pb-3 pt-3`}
+      className={`${className} rounded-full flex px-8 py-2 border border-gray-400 sm:text-sm items-center font-medium
+      ${success && "text-black bg-green-500 "}
+      ${primary && "text-white bg-blue-500 hover:bg-blue-600"}
+      `}
     >
-      <span className="pr-1">{icon}</span>
-      <span>{buttonName}</span>
+      <span className="pr-1 hidden sm:block">{icon}</span>
+      {buttonName}
     </button>
   );
 };
